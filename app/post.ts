98383c19c4cc5478278aa5e894b2a3052a846a47
Hani Husamuddin <hani.husam@gmail.com>
2021-11-23 10:17:59 +0700

    feat: admin add new post

--- app/post.ts
+++ app/post.ts
@@ -1,18 +1,64 @@
-type Post = {
+import path from 'path'
+import fs from 'fs/promises'
+import parseFrontMatter from 'front-matter'
+import {processMarkdown} from '@ryanflorence/md'
+import invariant from 'tiny-invariant'
+
+export type Post = {
   slug: string
   title: string
 }
 
-export function getPosts() {
-  let posts: Post[] = [
-    {
-      slug: 'my-first-post',
-      title: 'My First Post',
-    },
-    {
-      slug: '90s-mixtape',
-      title: 'A Mixtape I Made Just For You',
-    },
-  ]
-  return posts
+type NewPost = {
+  title: string
+  slug: string
+  markdown: string
+}
+
+export type PostMarkdownAttributes = {
+  title: string
+}
+
+let postsPath = path.join(__dirname, '../posts')
+
+function isValidPostAttributes(
+  attributes: any,
+): attributes is PostMarkdownAttributes {
+  return attributes?.title
+}
+
+export async function getPosts() {
+  let dir = await fs.readdir(postsPath)
+  return Promise.all(
+    dir.map(async filename => {
+      let file = await fs.readFile(path.join(postsPath, filename))
+      let {attributes} = parseFrontMatter(file.toString())
+      invariant(
+        isValidPostAttributes(attributes),
+        `${filename} has bad meta data!`,
+      )
+      return {
+        slug: filename.replace(/\.md$/, ''),
+        title: attributes.title,
+      }
+    }),
+  )
+}
+
+export async function getPost(slug: string) {
+  let filepath = path.join(postsPath, slug + '.md')
+  let file = await fs.readFile(filepath)
+  let {attributes, body} = parseFrontMatter(file.toString())
+  invariant(
+    isValidPostAttributes(attributes),
+    `Post ${filepath} is missing attributes`,
+  )
+  let html = await processMarkdown(body)
+  return {slug, html, title: attributes.title}
+}
+
+export async function createPost(post: NewPost) {
+  let md = `---\ntitle: ${post.title}\n---\n\n${post.markdown}`
+  await fs.writeFile(path.join(postsPath, post.slug + '.md'), md)
+  return getPost(post.slug)
 }
---
+++ posts/try-new-post.md
@@ -0,0 +1,5 @@
+---
+title: Try New Post
+---
+
+This is a new post example.
